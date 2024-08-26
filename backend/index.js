1362031e5dfa0768251a8adf422c95f7ca588d90
Duncan MacKenzie <duncan@oxd.com>
2024-08-26 11:52:22 -0700

    Query db

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
-// index.mjs
 import http from "http";
+import pgp from "pg-promise";
 
-const server = http.createServer((req, res) => {
+if (!process.env.PG_CONNECTION_STRING) {
+  throw new Error("Required environment variables are not set");
+}
+
+const db = pgp()(process.env.PG_CONNECTION_STRING);
+
+const server = http.createServer(async (req, res) => {
+  const dbRecords = await db.any("SELECT * FROM example_table");
+
   res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ hello: "world", time: new Date() }));
+  res.end(JSON.stringify({ hello: "world", time: new Date(), dbRecords }));
 });
 
 const PORT = 3000;
