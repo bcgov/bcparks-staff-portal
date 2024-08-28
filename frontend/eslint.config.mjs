1f0f2ed99e7de1084595cb7ab915cce96a29e462
Duncan MacKenzie <duncan@oxd.com>
2024-08-28 15:14:12 -0700

    Update eslint settings

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,12 +1,22 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
+import eslintConfigESLint from "eslint-config-eslint";
+import eslintConfigESLintFormatting from "eslint-config-eslint/formatting";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
+  ...eslintConfigESLint,
+  eslintConfigESLintFormatting,
   pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
+
+  // override rules
+  {
+    rules: {
+      // Allow console.warn and console.error
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
 ];
