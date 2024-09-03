dfdccee18ef458f59fac36ae96aacf466712b7e9
Duncan MacKenzie <duncan@oxd.com>
2024-09-03 14:15:00 -0700

    Install Redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-// import store from "./store";
-// import { Provider } from "react-redux";
-
+import store from "./store";
+import { Provider } from "react-redux";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 );
--- frontend/src/router/pages/Bar.jsx
+++ frontend/src/router/pages/Bar.jsx
@@ -1,7 +1,33 @@
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../../features/counter/counterSlice";
+
 export default function BarPage() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div id="bar page">
       <h1>Bar page!</h1>
+
+      <h2>Test the counter store:</h2>
+
+      <div>
+        <span>count = {count}</span>
+        &nbsp;
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        &nbsp;
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
