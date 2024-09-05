26eae50d9717e98907d6b977907f19380a25f3f6
Duncan MacKenzie <duncan@oxd.com>
2024-09-04 18:58:15 -0700

    Remove unused stylesheet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import store from "./store";
 import { Provider } from "react-redux";
-import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
