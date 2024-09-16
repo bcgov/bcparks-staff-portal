7d0a1a6f24ad634b8478a58229505c45ecc3f049
Duncan MacKenzie <duncan@oxd.com>
2024-09-16 13:15:44 -0700

    Use getEnv function for env vars

--- frontend/src/config/keycloak.js
+++ frontend/src/config/keycloak.js
@@ -1,8 +1,10 @@
+import getEnv from "./getEnv";
+
 export default {
-  authority: import.meta.env.VITE_OIDC_AUTHORITY,
-  client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
-  redirect_uri: import.meta.env.VITE_OIDC_LOGIN_REDIRECT,
-  post_logout_redirect_uri: import.meta.env.VITE_OIDC_LOGOUT_REDIRECT,
+  authority: getEnv("VITE_OIDC_AUTHORITY"),
+  client_id: getEnv("VITE_OIDC_CLIENT_ID"),
+  redirect_uri: getEnv("VITE_OIDC_LOGIN_REDIRECT"),
+  post_logout_redirect_uri: getEnv("VITE_OIDC_LOGOUT_REDIRECT"),
 
   // Automatically renew the access token before it expires
   automaticSilentRenew: true,
