44a408172eab112111fa79bf69c921ff94bb4f07
Duncan MacKenzie <duncan@oxd.com>
2024-10-03 17:27:59 -0700

    Install design tokens package

--- frontend/src/router/pages/ApiTest.jsx
+++ frontend/src/router/pages/ApiTest.jsx
@@ -39,17 +39,38 @@ function ApiTest() {
 
   return (
     <>
-      <div className="card">
-        <button disabled={loading} onClick={() => getData(1)}>
-          🔒 Get DB row 1
-        </button>
-        &nbsp;
-        <button disabled={loading} onClick={() => getData(2)}>
-          🔒 Get DB row 2
-        </button>
-        {loading && <p>Loading...</p>}
-        {!loading && apiData && <p>{apiData}</p>}
-        {!loading && !apiData && <p>Click a button to test the API</p>}
+      <h1>API test page</h1>
+
+      <div className="card gap-2">
+        <div className="row">
+          <div className="col-6 d-grid">
+            <button
+              className="btn btn-primary"
+              disabled={loading}
+              onClick={() => getData(1)}
+            >
+              🔒 Get DB row 1
+            </button>
+          </div>
+          <div className="col-6 d-grid">
+            <button
+              className="btn btn-primary"
+              disabled={loading}
+              onClick={() => getData(2)}
+            >
+              🔒 Get DB row 2
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="row-12">
+            {loading && <p className="mb-0">Loading...</p>}
+            {!loading && apiData && <p className="mb-0">{apiData}</p>}
+            {!loading && !apiData && (
+              <p className="mb-0">Click a button to test the API</p>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
