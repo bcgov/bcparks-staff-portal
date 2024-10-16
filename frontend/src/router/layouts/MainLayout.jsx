b44bdadf0a571c37bf86271628fc30c9aafeae83
Duncan MacKenzie <duncan@oxd.com>
2024-10-15 17:12:58 -0700

    Update page header styles

--- frontend/src/router/layouts/MainLayout.jsx
+++ frontend/src/router/layouts/MainLayout.jsx
@@ -26,7 +26,10 @@ export default function MainLayout() {
         </header>
 
         <main className="container">
-          <nav>
+          <Outlet />
+
+          {/* Temporary placeholder nav */}
+          <nav className="mt-5">
             <ul style={{ listStyle: "none" }}>
               <li>
                 <Link to={`/`}>Home page</Link>
@@ -36,8 +39,6 @@ export default function MainLayout() {
               </li>
             </ul>
           </nav>
-
-          <Outlet />
         </main>
 
         <footer className="bcparks-global py-2 py-md-0 d-flex justify-content-md-end align-items-center container-fluid text-bg-primary-nav">
