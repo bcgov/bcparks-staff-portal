0e1cb4d15605c7c5b0b4c86ad3e2d61d02cebf6e
Duncan MacKenzie <duncan@oxd.com>
2024-12-06 09:54:26 -0800

    CMS-460: Redirect back to the same page on login (#45)

--- frontend/src/config/keycloak.js
+++ frontend/src/config/keycloak.js
@@ -3,12 +3,15 @@ import getEnv from "./getEnv";
 export default {
   authority: getEnv("VITE_OIDC_AUTHORITY"),
   client_id: getEnv("VITE_OIDC_CLIENT_ID"),
-  redirect_uri: getEnv("VITE_OIDC_LOGIN_REDIRECT"),
-  post_logout_redirect_uri: getEnv("VITE_OIDC_LOGOUT_REDIRECT"),
+
+  // Redirect back to the page you were on after logging in
+  redirect_uri: `${window.location.origin}${window.location.pathname}`,
+  post_logout_redirect_uri: window.location.origin,
 
   // Automatically renew the access token before it expires
   automaticSilentRenew: true,
 
+  // Strips OIDC parameters from the URL after redirecting back to the app
   onSigninCallback() {
     window.history.replaceState({}, document.title, window.location.pathname);
   },
