9017178041386f5a0c97b28cba468bac188cebfc
Duncan MacKenzie <duncan@oxd.com>
2024-12-18 15:22:26 -0800

    CMS-322: Fix operating year check

--- frontend/src/hooks/useValidation.js
+++ frontend/src/hooks/useValidation.js
@@ -155,12 +155,12 @@ export default function useValidation(dates, notes, season) {
     const operatingYear = season.operatingYear;
 
     // Date must be within the year for that form
-    if (
-      startDate.getFullYear() !== operatingYear ||
-      endDate.getFullYear() !== operatingYear
-    ) {
-      // startDate =< endDate check happens first, so the end date will never fail this check
+    if (startDate.getFullYear() !== operatingYear) {
       return addError(startDateId, `Enter dates for ${operatingYear} only`);
+    }
+
+    if (endDate.getFullYear() !== operatingYear) {
+      return addError(endDateId, `Enter dates for ${operatingYear} only`);
     }
 
     return true;
