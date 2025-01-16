d4b535305a44e29dc5a7f6033279d75b1397ac57
Diego Ramirez <diegoramirez@Diegos-MacBook-Pro.local>
2025-01-16 11:52:15 -0800

    CMS-338: Update strapi URL to use env var during sync.

--- backend/strapi-sync/sync.js
+++ backend/strapi-sync/sync.js
@@ -77,7 +77,7 @@ export async function getData(url, queryParams) {
  * @returns {Array} list of all models with thier name, endpoint, and items
  */
 export async function fetchAllModels() {
-  const url = "https://cms.bcparks.ca/api";
+  const url = `${process.env.STRAPI_TOKEN}/api`;
 
   const strapiData = [
     {
@@ -598,7 +598,7 @@ async function createTestUser() {
  */
 export async function oneTimeDataImport() {
   // only meant to run once - not needed for regular sync
-  const url = "https://cms.bcparks.ca/api";
+  const url = `${process.env.STRAPI_TOKEN}/api`;
 
   const datesData = {
     endpoint: "/park-operation-sub-area-dates",
