991156be1a68cff4321769c91a7221f47615dd19
Duncan MacKenzie <duncan@oxd.com>
2025-02-07 15:39:44 -0800

    CMS-654: Remove unused store and uninstall redux (#110)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import store from "./store";
-import { Provider as StoreProvider } from "react-redux";
 import { AuthProvider } from "react-oidc-context";
 import oidcConfig from "./config/keycloak.js";
 
@@ -13,9 +11,7 @@ import "@/styles/global.scss";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider {...oidcConfig}>
-      <StoreProvider store={store}>
-        <RouterProvider router={router} />
-      </StoreProvider>
+      <RouterProvider router={router} />
     </AuthProvider>
   </StrictMode>,
 );
