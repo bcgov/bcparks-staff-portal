4637980630201033bbad954a4b82525d63b1269f
diego-oxd <107205353+diego-oxd@users.noreply.github.com>
2025-03-28 08:46:17 -0700

    CMS-779: Update logic of how winter dates are created (#175)
    * CMS-779: Update logic of how winter dates are created
    
    * CMS-779: Only create 2025 seasons if 2024 season exists
    
    * CMS-779: Small fix. Add await in sync script

--- backend/strapi-sync/create-missing-dates-and-seasons.js
+++ backend/strapi-sync/create-missing-dates-and-seasons.js
@@ -7,54 +7,69 @@ import {
   DateRange,
 } from "../models/index.js";
 import { createModel } from "./utils.js";
+import { Op } from "sequelize";
 
 export async function createMissingDatesAndSeasons() {
-  const [seasons2025, dateTypes, winterFeatureType, features] =
-    await Promise.all([
-      Season.findAll({
-        where: { operatingYear: 2025 },
-        attributes: ["id", "operatingYear", "parkId", "featureTypeId"],
-      }),
-      DateType.findAll(),
-      FeatureType.findOne({
-        attributes: ["id", "name"],
-        where: { name: "Winter fee" },
-      }),
-      Feature.findAll({
-        attributes: [
-          "id",
-          "name",
-          "strapiId",
-          "parkId",
-          "featureTypeId",
-          "hasReservations",
-          "hasWinterFeeDates",
-        ],
-        include: [
-          {
-            model: Dateable,
-            as: "dateable",
-            attributes: ["id"],
-            include: [
-              {
-                model: DateRange,
-                as: "dateRanges",
-                attributes: [
-                  "id",
-                  "startDate",
-                  "endDate",
-                  "seasonId",
-                  "dateTypeId",
-                ],
-              },
-            ],
-          },
-        ],
-        where: {
-          active: true,
+  const [seasons, dateTypes, winterFeatureType, features] = await Promise.all([
+    // get all seasons for 2025 and 2024
+    Season.findAll({
+      attributes: ["id", "operatingYear", "parkId", "featureTypeId"],
+      where: {
+        operatingYear: {
+          [Op.in]: [2024, 2025],
         },
-      }),
-    ]);
+      },
+    }),
+    DateType.findAll(),
+    FeatureType.findOne({
+      attributes: ["id", "name"],
+      where: { name: "Winter fee" },
+    }),
+    Feature.findAll({
+      attributes: [
+        "id",
+        "name",
+        "strapiId",
+        "parkId",
+        "featureTypeId",
+        "hasReservations",
+        "hasWinterFeeDates",
+      ],
+      include: [
+        {
+          model: Dateable,
+          as: "dateable",
+          attributes: ["id"],
+          include: [
+            {
+              model: DateRange,
+              as: "dateRanges",
+              attributes: [
+                "id",
+                "startDate",
+                "endDate",
+                "seasonId",
+                "dateTypeId",
+              ],
+            },
+          ],
+        },
+      ],
+      where: {
+        active: true,
+      },
+    }),
+  ]);
+
+  const seasons2024 = seasons.filter((season) => season.operatingYear === 2024);
+  const seasons2025 = seasons.filter((season) => season.operatingYear === 2025);
+
+  const seasonMap2024 = new Map(
+    seasons2024.map((season) => [
+      `${season.parkId}-${season.featureTypeId}`,
+      season,
+    ]),
+  );
 
   const seasonMap = new Map(
     seasons2025.map((season) => [
@@ -89,62 +104,76 @@ export async function createMissingDatesAndSeasons() {
   }
 
   for (const feature of features) {
-    const season = await getOrCreateSeason(
-      feature.parkId,
-      feature.featureTypeId,
-    );
+    const key = `${feature.parkId}-${feature.featureTypeId}`;
 
-    // existing dates for this feature-season
-    const existingDateTypes = new Set(
-      feature.dateable.dateRanges
-        .filter((dateRange) => dateRange.seasonId === season.id)
-        .map((dateRange) => dateRange.dateTypeId),
-    );
+    // get 2024 season
+    const season2024 = seasonMap2024.get(key);
 
-    // add missing operation dates
-    if (!existingDateTypes.has(dateTypeMap.get("Operation")?.id)) {
-      datesToCreate.push({
-        startDate: null,
-        endDate: null,
-        dateTypeId: dateTypeMap.get("Operation").id,
-        dateableId: feature.dateable.id,
-        seasonId: season.id,
-      });
-    }
+    if (season2024) {
+      const season = await getOrCreateSeason(
+        feature.parkId,
+        feature.featureTypeId,
+      );
 
-    // add missing reservation dates
-    if (
-      feature.hasReservations &&
-      !existingDateTypes.has(dateTypeMap.get("Reservation")?.id)
-    ) {
-      datesToCreate.push({
-        startDate: null,
-        endDate: null,
-        dateTypeId: dateTypeMap.get("Reservation").id,
-        dateableId: feature.dateable.id,
-        seasonId: season.id,
-      });
+      // existing dates for this feature-season
+      const existingDateTypes = new Set(
+        feature.dateable.dateRanges
+          .filter((dateRange) => dateRange.seasonId === season.id)
+          .map((dateRange) => dateRange.dateTypeId),
+      );
+
+      // add missing operation dates
+      if (!existingDateTypes.has(dateTypeMap.get("Operation")?.id)) {
+        datesToCreate.push({
+          startDate: null,
+          endDate: null,
+          dateTypeId: dateTypeMap.get("Operation").id,
+          dateableId: feature.dateable.id,
+          seasonId: season.id,
+        });
+      }
+
+      // add missing reservation dates
+      if (
+        feature.hasReservations &&
+        !existingDateTypes.has(dateTypeMap.get("Reservation")?.id)
+      ) {
+        datesToCreate.push({
+          startDate: null,
+          endDate: null,
+          dateTypeId: dateTypeMap.get("Reservation").id,
+          dateableId: feature.dateable.id,
+          seasonId: season.id,
+        });
+      }
     }
 
     // add winter fee seasons
     if (feature.hasWinterFeeDates) {
-      const winterSeason = await getOrCreateSeason(
-        feature.parkId,
-        winterFeatureType.id,
-      );
+      const winterKey = `${feature.parkId}-${winterFeatureType.id}`;
 
-      const existingWinterDates = feature.dateable.dateRanges.filter(
-        (dateRange) => dateRange.seasonId === winterSeason.id,
-      );
+      // get 2024 winter season
+      const winterSeason2024 = seasonMap2024.get(winterKey);
 
-      if (existingWinterDates.length === 0) {
-        datesToCreate.push({
-          startDate: null,
-          endDate: null,
-          dateTypeId: dateTypeMap.get("Winter fee").id,
-          dateableId: feature.dateable.id,
-          seasonId: winterSeason.id,
-        });
+      if (winterSeason2024) {
+        const winterSeason = await getOrCreateSeason(
+          feature.parkId,
+          winterFeatureType.id,
+        );
+
+        const existingWinterDates = feature.dateable.dateRanges.filter(
+          (dateRange) => dateRange.seasonId === winterSeason.id,
+        );
+
+        if (existingWinterDates.length === 0) {
+          datesToCreate.push({
+            startDate: null,
+            endDate: null,
+            dateTypeId: dateTypeMap.get("Winter fee").id,
+            dateableId: feature.dateable.id,
+            seasonId: winterSeason.id,
+          });
+        }
       }
     }
   }
