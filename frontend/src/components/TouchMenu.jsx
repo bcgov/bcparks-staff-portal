83b44d1a786ea4bde4a878a11603b50db77f3e3a
Duncan MacKenzie <duncan@oxd.com>
2025-04-30 16:26:25 -0700

    CMS-634: Add touch menu for small screens

---
+++ frontend/src/components/TouchMenu.jsx
@@ -0,0 +1,88 @@
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import "./TouchMenu.scss";
+
+const navItems = [
+  {
+    label: "Advisories",
+    Tag: "a",
+    href: "/advisories",
+  },
+  {
+    label: "Park Access Status",
+    Tag: "a",
+    href: "/park-access-status",
+  },
+  {
+    label: "Activities & Facilities",
+    Tag: "a",
+    href: "/activities-and-facilities",
+  },
+  {
+    label: "Dates of Operation",
+    Tag: Link,
+    to: "/",
+    // This menu only displays on DOOT right now,
+    // so Dates of Operation is always the active item.
+    active: true,
+  },
+];
+
+export default function TouchMenu({
+  show = true,
+  closeMenu,
+  logOut,
+  userName,
+}) {
+  function onLogoutClick() {
+    closeMenu();
+    logOut();
+  }
+
+  return (
+    <div
+      className={classNames("navbar-collapse collapse d-md-none", {
+        show,
+      })}
+      id="touch-menu"
+    >
+      <ul className="navbar-nav">
+        {navItems.map(({ Tag, label, active, ...itemProps }) => (
+          <li key={label} className="nav-item">
+            {/* Use "a" for external links and Router Link components for internal links */}
+            <Tag
+              className={classNames("nav-link", {
+                active,
+              })}
+              {...itemProps}
+              onClick={closeMenu}
+            >
+              {label}
+            </Tag>
+          </li>
+        ))}
+        <li className="nav-item">
+          {/* Full width logout button with the user's name */}
+          <button
+            type="button"
+            className="btn btn-text w-100 text-start nav-link"
+            onClick={onLogoutClick}
+          >
+            {userName}
+            <br />
+            Logout
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+// Prop validation
+TouchMenu.propTypes = {
+  show: PropTypes.bool,
+  closeMenu: PropTypes.func.isRequired,
+  logOut: PropTypes.func.isRequired,
+  userName: PropTypes.string,
+};
