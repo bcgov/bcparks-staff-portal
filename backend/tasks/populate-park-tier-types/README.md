7b470b1bbcc3b925857599f5073fd83cf1988199
Duncan MacKenzie <duncan@oxd.com>
2025-05-27 17:20:27 -0700

    CMS-846: Add script

---
+++ backend/tasks/populate-park-tier-types/README.md
@@ -0,0 +1,8 @@
+# Populate Park Tier1/Tier2 flags
+
+The Park model has 2 columns: `hasTier1Dates` and `hasTier1Dates` that indicate if the Park has Tier1/Tier2 dates.
+This script sets the flags based on provided JSON data.
+
+This script is designed to run one time in each environment, to intially populate the fields. If the data changes, you can reset all the parks to "false" and re-run the script with a new JSON file.
+
+Small updates can be done through AdminJS.
---
+++ backend/tasks/populate-park-tier-types/populate-park-tier-types.js
@@ -0,0 +1,43 @@
+import fs from "node:fs";
+import path from "node:path";
+import { Park } from "../../models/index.js";
+
+// Load JSON data
+const jsonPath = path.join(import.meta.dirname, "2025-tier-data.json");
+const jsonData = JSON.parse(fs.readFileSync(jsonPath, "utf8"));
+
+// Update each  park
+const updateQueries = jsonData.map(async (parkData) => {
+  const { orcs, tierType } = parkData;
+  const columnName = tierType === "t1" ? "hasTier1Dates" : "hasTier2Dates";
+
+  try {
+    const park = await Park.findOne({
+      where: { orcs },
+    });
+
+    if (!park) {
+      throw new Error(`Park with orcs ${orcs} not found.`);
+    }
+
+    // Update the
+    const updateQuery = await park.update({
+      [columnName]: true,
+      where: {
+        orcs,
+      },
+    });
+
+    console.log(`Updated park ${orcs} with tierType ${tierType}`);
+
+    return updateQuery;
+  } catch (error) {
+    console.error(`Error updating park ${orcs}:`, error);
+  }
+
+  return null;
+});
+
+await Promise.all(updateQueries);
+
+console.log("Done");
