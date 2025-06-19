b148b7095c166ef75a407b0cb548bf27ec6e3571
Duncan MacKenzie <duncan@oxd.com>
2025-06-19 15:17:22 -0700

    CMS-909: Edit forms for all 3 levels; remove unused v1 pages

---
+++ frontend/src/components/ChangeLogsList.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import { formatDate } from "@/lib/utils";
+
+// import "./ChangeLogsList.scss";
+
+// Displays season date changelogs (aka "notes")
+export default function ChangeLogsList({ changeLogs = [] }) {
+  return (
+    <div className="change-logs-list">
+      {changeLogs.map((changeLog) => (
+        <p key={changeLog.id}>
+          {changeLog.notes && (
+            <span>
+              {changeLog.notes}
+              <br />
+            </span>
+          )}
+          <span className="note-metadata">
+            {formatDate(changeLog.createdAt, "America/Vancouver")} by{" "}
+            {changeLog.user.name}
+          </span>
+        </p>
+      ))}
+    </div>
+  );
+}
+
+// Prop validation
+ChangeLogsList.propTypes = {
+  changeLogs: PropTypes.array,
+};
