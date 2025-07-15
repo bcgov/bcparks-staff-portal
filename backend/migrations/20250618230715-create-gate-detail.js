197c48541155d94a18b4e206aaf0057f1aeac99f
Ayumi Tanaka <ayumi@oxd.com>
2025-07-15 15:46:39 -0700

    CMS-907: Update gate detail migration

--- backend/migrations/20250618230715-create-gate-detail.js
+++ backend/migrations/20250618230715-create-gate-detail.js
@@ -44,6 +44,11 @@ module.exports = {
         allowNull: false,
         defaultValue: false,
       },
+      isTimeRangeAnnual: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
