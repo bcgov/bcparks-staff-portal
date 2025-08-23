7f09b82b983c08adcbffbc48774558c855522ad2
Ayumi Tanaka <ayumi@oxd.com>
2025-08-22 23:35:55 -0700

    CMS-1132: Change an order of scripts

--- backend/tasks/re-sync-all/re-sync-all.js
+++ backend/tasks/re-sync-all/re-sync-all.js
@@ -18,91 +18,111 @@ import { populatePreviousDates } from "../populate-previous-dates/populate-previ
 const execAsync = promisify(exec);
 
 async function reSyncAll() {
+  let step = 1;
+
   try {
     // 1 - delete all data but not User table
-    console.log("Starting: 1 - Delete all data...");
+    console.log(`Starting: ${step} - Delete all data...`);
     await deleteAllData();
-    console.log("Finished: 1 - Delete all data.\n");
+    console.log(`Finished: ${step} - Delete all data.\n`);
+    step++;
 
     // 2 - import Feature, FeatureType, Park, ParkArea, ManagementArea, Section from Strapi
     // TODO: split this "importData" out into a separate script in tasks
     console.log(
-      "Starting: 2 - Import Feature, FeatureType, Park, ParkArea, ManagementArea, Section from Strapi...",
+      `Starting: ${step} - Import Feature, FeatureType, Park, ParkArea, ManagementArea, Section from Strapi...`,
     );
     await importData();
     console.log(
-      "Finished: 2 - Import Feature, FeatureType, Park, ParkArea, ManagementArea, Section from Strapi.\n",
+      `Finished: ${step} - Import Feature, FeatureType, Park, ParkArea, ManagementArea, Section from Strapi.\n`,
     );
+    step++;
 
     // 3 - import DateType from date-types.js
-    console.log("Starting: 3 - Import DateType...");
+    console.log(`Starting: ${step} - Import DateType...`);
     await createDateTypes();
-    console.log("Finished: 3 - Import DateType.\n");
+    console.log(`Finished: ${step} - Import DateType.\n`);
+    step++;
 
     // 4 - populate "hasWinterFeeDates" in Park from 2025-winter-parks.json
-    console.log("Starting: 4 - Populate hasWinterFeeDates in Park...");
+    console.log(`Starting: ${step} - Populate hasWinterFeeDates in Park...`);
     await populateParkWinterFeeFlag();
-    console.log("Finished: 4 - Populate hasWinterFeeDates in Park.\n");
+    console.log(`Finished: ${step} - Populate hasWinterFeeDates in Park.\n`);
+    step++;
 
     // 5 - populate "hasTier1Dates" and "hasTier2Dates" in Park from 2025-tier-data.json
     console.log(
-      "Starting: 5 - Populate hasTier1Dates and hasTier2Dates in Park...",
+      `Starting: ${step} - Populate hasTier1Dates and hasTier2Dates in Park...`,
     );
     await populateParkTierTypes();
     console.log(
-      "Finished: 5 - Populate hasTier1Dates and hasTier2Dates in Park.\n",
+      `Finished: ${step} - Populate hasTier1Dates and hasTier2Dates in Park.\n`,
     );
+    step++;
 
     // 6 - populate "inReservationSystem" in ParkArea
-    console.log("Starting: 6 - Populate inReservationSystem in ParkArea...");
+    console.log(
+      `Starting: ${step} - Populate inReservationSystem in ParkArea...`,
+    );
     await populateParkAreaInReservationSystem();
-    console.log("Finished: 6 - Populate inReservationSystem in ParkArea.\n");
+    console.log(
+      `Finished: ${step} - Populate inReservationSystem in ParkArea.\n`,
+    );
+    step++;
 
     // 7 - import AccessGroup, AccessGroupPark, UserAccessGroup from agreements-with-usernames.json
     console.log(
-      "Starting: 7 - Import AccessGroup, AccessGroupPark, UserAccessGroup...",
+      `Starting: ${step} - Import AccessGroup, AccessGroupPark, UserAccessGroup...`,
     );
     await populateAccessGroups();
     console.log(
-      "Finished: 7 - Import AccessGroup, AccessGroupPark, UserAccessGroup.\n",
+      `Finished: ${step} - Import AccessGroup, AccessGroupPark, UserAccessGroup.\n`,
     );
+    step++;
 
-    // 8 - import GateDetail from Strapi park-operation and park-operation-sub-area
-    console.log("Starting: 8 - Import GateDetail from Strapi...");
-    await createGateDetailsFromStrapi();
-    console.log("Finished: 8 - Import GateDetail from Strapi.\n");
-
-    // 9 - create 2026 Seasons
-    console.log("Starting: 9 - Create blank seasons for 2026...");
+    // 8 - create 2026 Seasons
+    console.log(`Starting: ${step} - Create blank seasons for 2026...`);
     await execAsync("node ./tasks/create-seasons.js 2026");
-    console.log("Finished: 9 - Create blank seasons for 2026.\n");
+    console.log(`Finished: ${step} - Create blank seasons for 2026.\n`);
+    step++;
 
-    // 10 - import Season and DateRange from Strapi park-operation-sub-area-dates
-    console.log("Starting: 10 - Import sub-area dates...");
+    // 9 - import Season and DateRange from Strapi park-operation-sub-area-dates
+    console.log(`Starting: ${step} - Import sub-area dates...`);
     await importSubAreaDates();
-    console.log("Finished: 10 - Import sub-area dates.\n");
+    console.log(`Finished: ${step} - Import sub-area dates.\n`);
+    step++;
 
-    // 11 - import Season and DateRange from Strapi park-feature-dates
-    console.log("Starting: 11 - Import park feature dates...");
+    // 10 - import Season and DateRange from Strapi park-feature-dates
+    console.log(`Starting: ${step} - Import park feature dates...`);
     await importParkFeatureDates();
-    console.log("Finished: 11 - Import park feature dates.\n");
+    console.log(`Finished: ${step} - Import park feature dates.\n`);
+    step++;
 
-    // 12 - import Season and DateRange from Strapi park-operation-dates
-    console.log("Starting: 12 - Import park gate dates...");
+    // 11 - import Season and DateRange from Strapi park-operation-dates
+    console.log(`Starting: ${step} - Import park gate dates...`);
     await populateParkGateDates();
-    console.log("Finished: 12 - Import park gate dates.\n");
+    console.log(`Finished: ${step} - Import park gate dates.\n`);
+    step++;
 
-    // 13 - import Season and DateRange from previous-dates.json
-    console.log("Starting: 13 - Import previous dates...");
+    // 12 - import Season and DateRange from previous-dates.json
+    console.log(`Starting: ${step} - Import previous dates...`);
     await populatePreviousDates();
-    console.log("Finished: 13 - Import previous dates.\n");
+    console.log(`Finished: ${step} - Import previous dates.\n`);
+    step++;
 
-    // 14 - import DateRangeAnnual from Strapi park-operation
-    console.log("Starting: 14 - Import DateRangeAnnual...");
+    // 13 - import DateRangeAnnual from Strapi park-operation
+    console.log(`Starting: ${step} - Import DateRangeAnnual...`);
     await createDateRangeAnnualEntries();
-    console.log("Finished: 14 - Import DateRangeAnnual.\n");
+    console.log(`Finished: ${step} - Import DateRangeAnnual.\n`);
+    step++;
 
-    console.log("All re-sync 14 steps completed.");
+    // 14 - import GateDetail from Strapi park-operation and park-operation-sub-area
+    console.log(`Starting: ${step} - Import GateDetail from Strapi...`);
+    await createGateDetailsFromStrapi();
+    console.log(`Finished: ${step} - Import GateDetail from Strapi.\n`);
+    step++;
+
+    console.log(`All re-sync ${step - 1} steps completed.`);
   } catch (err) {
     console.error("Error running scripts:", err);
   }
