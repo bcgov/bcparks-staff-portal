387992ac34fee9d42a304e8774e522853992f7df
Michael Olund <mike@oxd.com>
2025-09-29 10:37:11 -0700

    CMS-1261: Change message text (#349)

--- frontend/src/components/NoParkAccess.jsx
+++ frontend/src/components/NoParkAccess.jsx
@@ -2,14 +2,13 @@ export function NoParkAccess() {
   return (
     <div className="layout my-5">
       <div className="container">
-        <h1 className="mt-3 mb-4">Access pending</h1>
+        <h1 className="mt-3 mb-4">No parks found</h1>
         <p>
           You’ve successfully signed in, but your account hasn’t yet been
           granted access to any parks.
         </p>
         <p>
-          If this message continues to appear after a couple of days, please
-          contact{" "}
+          To add parks, contact{" "}
           <a href="mailto:parksweb@gov.bc.ca?subject=No%20park%20access%20assigned">
             parksweb@gov.bc.ca
           </a>
