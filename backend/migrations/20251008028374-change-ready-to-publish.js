cd46fbc1e2e4550a90038c14c3f4cbd1039fb215
Ayumi Tanaka <ayumi@oxd.com>
2025-10-30 18:23:43 -0700

    CMS-1219: Update all readyToPublish to true

--- backend/migrations/20251008028374-change-ready-to-publish.js
+++ backend/migrations/20251008028374-change-ready-to-publish.js
@@ -1,6 +1,12 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    // First, update all existing records to true
+    await queryInterface.sequelize.query(
+      'UPDATE "Seasons" SET "readyToPublish" = true WHERE "readyToPublish" IS NULL OR "readyToPublish" = false;'
+    );
+
+    // Then change the column definition
     await queryInterface.changeColumn("Seasons", "readyToPublish", {
       type: Sequelize.BOOLEAN,
       allowNull: false,
@@ -9,10 +15,16 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
+    // Revert column definition
     await queryInterface.changeColumn("Seasons", "readyToPublish", {
       type: Sequelize.BOOLEAN,
       allowNull: false,
       defaultValue: false,
     });
+
+    // Optionally revert all values back to false
+    await queryInterface.sequelize.query(
+      'UPDATE "Seasons" SET "readyToPublish" = false;'
+    );
   },
 };
