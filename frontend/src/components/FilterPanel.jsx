b5e553f7b5e7f2f88393415d606cf0e801b64036
Mike Olund <mike@oxd.com>
2025-11-17 12:34:53 -0800

    CMS-915: Change button label on the filter panel

--- frontend/src/components/FilterPanel.jsx
+++ frontend/src/components/FilterPanel.jsx
@@ -15,6 +15,7 @@ function FilterPanel({
   filterOptionsError,
   statusFilter,
   clearFilter,
+  totalItems,
 }) {
   // constants and states
   const { sections, managementAreas, dateTypes, featureTypes, accessGroups } =
@@ -137,7 +138,8 @@ function FilterPanel({
         )}
         <div className="mt-4">
           <button className="btn btn-primary" onClick={() => handleClose()}>
-            Apply filters
+            Show {totalItems} park
+            {totalItems !== 1 ? "s" : ""}
           </button>
           {clearFilter}
         </div>
@@ -172,4 +174,5 @@ FilterPanel.propTypes = {
   filterOptionsError: PropTypes.object,
   statusFilter: PropTypes.element.isRequired,
   clearFilter: PropTypes.element.isRequired,
+  totalItems: PropTypes.number.isRequired,
 };
--- frontend/src/router/pages/EditAndReview.jsx
+++ frontend/src/router/pages/EditAndReview.jsx
@@ -530,6 +530,7 @@ function EditAndReview() {
           filterOptionsError={filterOptionsError}
           statusFilter={<StatusFilter />}
           clearFilter={<ClearFilter />}
+          totalItems={filteredParks.length}
         />
       </div>
     </div>
