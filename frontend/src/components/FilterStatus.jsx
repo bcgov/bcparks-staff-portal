9b9efabc7088729788ba94287b25f2bb8c0dfc44
Duncan MacKenzie <duncan@oxd.com>
2025-12-04 16:39:13 -0800

    CMS-1028: Add FilterStatus component

---
+++ frontend/src/components/FilterStatus.jsx
@@ -0,0 +1,44 @@
+export default function FilterStatus({ filters, filteredCount, ClearFilters }) {
+  return (
+    <div className="filter-status">
+      <div className="num-results mb-3">{filteredCount} results</div>
+
+      <div className="active-filters mb-3 d-flex flex-row flex-wrap gap-2 align-items-center">
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+
+        <ClearFilters />
+      </div>
+    </div>
+  );
+}
