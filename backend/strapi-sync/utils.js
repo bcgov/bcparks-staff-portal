f76fd996576a74ba9df01368cff48de7918f201e
Mike Olund <mike@oxd.com>
2026-01-06 17:25:29 -0800

    CMS-1339: Initial work on Strapi to DOOT sync automation

--- backend/strapi-sync/utils.js
+++ backend/strapi-sync/utils.js
@@ -1,6 +1,3 @@
-import { ParkArea, Park } from "../models/index.js";
-import { getStrapiModelData } from "./strapi-data-service.js";
-
 /**
  * Creates model in our local DB
  * @param {Model} model Sequelize model
@@ -12,66 +9,6 @@ export async function createModel(model, data) {
     return model.create(data);
   }
   return model.create();
-}
-
-/**
- * Gets the data for a specific model by id that we recently fetched from strapi
- * @param {Object} strapiData data for every model from strapi
- * @param {string} modelName the name of the model we want to retrieve from
- * @param {number} id the id of the model we want to retrieve
- * @returns {Object} the data for the specific model instance we're looking for
- */
-export async function getStrapiModelDataById(modelName, id) {
-  const modelData = await getStrapiModelData(modelName);
-
-  if (!modelData) {
-    return null;
-  }
-
-  return modelData.items.find((item) => item.id === id);
-}
-
-/**
- * Gets the icon for the feature type based on the camping type or facility type
- * @param {Array} strapiData data for every model from strapi
- * @param {Object} featureType the feature type we want to get the icon for
- * @returns {string} the icon for the feature type
- */
-export async function getFeatureTypeIcon(featureType) {
-  if (featureType.campingType.data !== null) {
-    const campingType = await getStrapiModelDataById(
-      "camping-type",
-      featureType.campingType.data.id,
-    );
-
-    return campingType?.attributes.icon;
-  }
-  const facilityType = await getStrapiModelDataById(
-    "facility-type",
-    featureType.facilityType.data.id,
-  );
-
-  return facilityType?.attributes.icon;
-}
-
-/**
- * Gets an items from the local DB by id
- * @param {Object} model Sequelize model
- * @param {number} id id of the item to get from DB
- * @returns {Promise<Model>} - Item with the given id
- */
-export async function getItemById(model, id) {
-  try {
-    const item = await model.findByPk(id);
-
-    if (!item) {
-      throw new Error(`Item with id ${id} not found`);
-    }
-    return item;
-  } catch (error) {
-    console.error(`Error fetching item by id: ${id}`, error);
-    throw error;
-  }
 }
 
 /**
@@ -97,40 +34,4 @@ export async function getItemByAttributes(model, attributes) {
     );
     throw error;
   }
-}
-
-/**
- * Finds or creates a ParkArea for a given parkArea item
- * @param {Object} item parkArea item from JSON data
- * @returns {Promise<Model>} A found or newly created ParkArea
- */
-export async function findOrCreateParkArea(item) {
-  // get park by orcs
-  const park = await getItemByAttributes(Park, {
-    orcs: String(item.orcs),
-  });
-
-  if (!park) {
-    throw new Error(`Park with ORCS ${item.orcs} not found`);
-  }
-
-  // check if ParkArea with the same name and parkId already exists
-  let parkArea = await ParkArea.findOne({
-    where: {
-      name: item.campgroundName,
-      parkId: park.id,
-    },
-  });
-
-  // if it doesn't exist, create it
-  if (!parkArea) {
-    const data = {
-      name: item.campgroundName,
-      parkId: park.id,
-    };
-
-    parkArea = await createModel(ParkArea, data);
-  }
-
-  return parkArea;
 }
