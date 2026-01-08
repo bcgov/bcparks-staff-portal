5977d6ccfd93bbd3dd1826bee72662f74c1ca4e6
Michael Olund <mike@oxd.com>
2026-01-07 17:11:38 -0800

    CMS-1339: Initial work on Strapi to DOOT sync automation (#415)

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 set -e
 
+MODE="$1"
+
+# Handle local dev volume mount structure vs Openshift volume structure
+if [ -d "/app/backend" ]; then
+  cd /app/backend
+fi
+
 echo "Running migrations..."
 npm run migrate
+
+# Run the cron task if specified
+if [ "$MODE" = "cron" ]; then
+  echo "Done. Running cron task..."
+  npm run cron-task
+  echo "Cron task complete."
+  exit 0
+fi
 
 # Start the backend server
 echo "Done. Starting the Express server..."
--- backend/strapi-sync/import-data.js
+++
@@ -1,5 +0,0 @@
-import { importData } from "./reset-and-import-data.js";
-
-// Initially populate the database with data from Strapi.
-// This is a one-time operation.
-importData();
