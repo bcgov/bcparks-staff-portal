0a5d1d36d3994400ce35ed0279b5a1596ce16110
Duncan MacKenzie <duncan@oxd.com>
2026-03-16 13:19:13 -0700

    CMS-1557: Update build process: build advisories in docker
    CMS-1557: Update Caddyfile, entrypoint
    
    CMS-1557: Fix docker syntax line

--- frontend/advisories/entrypoint.sh
+++ frontend/advisories/entrypoint.sh
@@ -33,15 +33,15 @@ fi
 # Populate env.js for the v2 frontend
 
 if [ -n "$VITE_OIDC_AUTHORITY" ]; then
-  echo "window.envVars.VITE_OIDC_AUTHORITY = \"$VITE_OIDC_AUTHORITY\";" >> $V2_APP_ROOT/env.js
+  echo "window.envVars.VITE_OIDC_AUTHORITY = \"$VITE_OIDC_AUTHORITY\";" >> $DOOT_APP_ROOT/env.js
 fi
 
 if [ -n "$VITE_OIDC_CLIENT_ID" ]; then
-  echo "window.envVars.VITE_OIDC_CLIENT_ID = \"$VITE_OIDC_CLIENT_ID\";" >> $V2_APP_ROOT/env.js
+  echo "window.envVars.VITE_OIDC_CLIENT_ID = \"$VITE_OIDC_CLIENT_ID\";" >> $DOOT_APP_ROOT/env.js
 fi
 
 if [ -n "$VITE_API_BASE_URL" ]; then
-  echo "window.envVars.VITE_API_BASE_URL = \"$VITE_API_BASE_URL\";" >> $V2_APP_ROOT/env.js
+  echo "window.envVars.VITE_API_BASE_URL = \"$VITE_API_BASE_URL\";" >> $DOOT_APP_ROOT/env.js
 fi
 
 # Start the server
