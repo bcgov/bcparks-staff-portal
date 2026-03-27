9c86083bde73936373e31080ea6d6b4b15dfcc3e
Duncan MacKenzie <duncan@oxd.com>
2026-03-26 17:09:17 -0700

    CMS-1585: Most basic ckeditor functionality
    CMS-1585: Update toolbar options
    
    CMS-1585: Use onBlur, replace previous field

--- frontend/src/components/advisories/composite/ckeditor/CKEditor.jsx
+++ frontend/src/components/advisories/composite/ckeditor/CKEditor.jsx
@@ -1,18 +1,29 @@
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import { ClassicEditor, Essentials, Paragraph, Bold, Italic } from "ckeditor5";
+import {
+  ClassicEditor,
+  Essentials,
+  Paragraph,
+  Bold,
+  Italic,
+  Link,
+  SourceEditing,
+} from "ckeditor5";
 import getEnv from "@/config/getEnv";
 
 import "ckeditor5/ckeditor5.css";
 
-export default function StaffPortalCKEditor() {
+export default function StaffPortalCKEditor({ value, onChange }) {
   return (
     <CKEditor
       editor={ClassicEditor}
+      data={value}
       config={{
         licenseKey: getEnv("VITE_CKEDITOR_LICENSE_KEY"),
-        plugins: [Essentials, Paragraph, Bold, Italic],
-        toolbar: ["undo", "redo", "|", "bold", "italic"],
-        initialData: "<p>Hello from CKEditor 5 in React!</p>",
+        plugins: [Essentials, Paragraph, Bold, Italic, Link, SourceEditing],
+        toolbar: ["bold", "italic", "|", "link", "|", "sourceEditing"],
+      }}
+      onBlur={(_, editor) => {
+        onChange(editor.getData());
       }}
     />
   );
