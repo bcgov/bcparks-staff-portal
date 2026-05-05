c875a68a0671e23c0e19e1c39bd2d66d77f9418c
Duncan MacKenzie <duncan@oxd.com>
2026-05-05 13:14:27 -0700

    CMS-1671: Fix typo

--- frontend/src/components/advisories/composite/advisoryForm/AdvisoryForm.jsx
+++ frontend/src/components/advisories/composite/advisoryForm/AdvisoryForm.jsx
@@ -454,7 +454,7 @@ export default function AdvisoryForm({
 
     // Super admins can specify the publishing status with an extra `advisoryStatus` field on the form
     if (hasAnyRole([ROLES.SUPER_ADMIN])) {
-      // Get the Strapi data for the advisry status from the selected value
+      // Get the Strapi data for the advisory status from the selected value
       publishedStatus = advisoryStatuses.find(
         (status) => status.value === advisoryStatus,
       );
