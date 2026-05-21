fa43181d4f531b67ca4d21fef5eb9f4bf841f2b8
Michael Olund <mike@oxd.com>
2026-05-21 12:44:27 -0700

    CMS-1683: Rename and remove css classes beginning with "ad-" (#572)
    * CMS-1683: Rename and remove css classes beginning with "ad-"
    
    * Change adv- to act-

--- frontend/src/components/advisories/composite/advisoryForm/AdvisoryForm.jsx
+++ frontend/src/components/advisories/composite/advisoryForm/AdvisoryForm.jsx
@@ -362,7 +362,7 @@ export default function AdvisoryForm({
 
     return (
       <div
-        className={`ad-helper-text ${isError ? "text-danger" : ""} ${className}`.trim()}
+        className={`act-helper-text ${isError ? "text-danger" : ""} ${className}`.trim()}
       >
         {text}
       </div>
@@ -911,7 +911,7 @@ export default function AdvisoryForm({
                       Browse
                     </Btn>
                     {linkFileErrors[idx] && (
-                      <span className="d-block text-danger ad-helper-text">
+                      <span className="d-block text-danger act-helper-text">
                         Select a file
                       </span>
                     )}
@@ -1576,7 +1576,7 @@ export default function AdvisoryForm({
 
       {!hasAnyRole([ROLES.ADVISORY_PUBLISH_WITHOUT_APPROVAL]) &&
         (isStatHoliday || isAfterHours) && (
-          <section className="ad-af-hour-box d-flex field-bg-blue">
+          <section className="act-af-hour-box d-flex field-bg-blue">
             <FontAwesomeIcon
               icon={faTriangleExclamation}
               className="warningIcon"
--- frontend/src/components/advisories/composite/advisorySummaryView/AdvisorySummaryView.jsx
+++ frontend/src/components/advisories/composite/advisorySummaryView/AdvisorySummaryView.jsx
@@ -147,7 +147,7 @@ export default function AdvisorySummaryView({
 
               <button
                 type="button"
-                className="ad-copy bcgov-button bcgov-normal-white"
+                className="act-copy bcgov-button bcgov-normal-white"
                 onClick={() => {
                   navigator.clipboard
                     .writeText(parkUrls)
@@ -209,7 +209,7 @@ export default function AdvisorySummaryView({
 
               <button
                 type="button"
-                className="ad-copy bcgov-button bcgov-normal-white"
+                className="act-copy bcgov-button bcgov-normal-white"
                 onClick={() => {
                   navigator.clipboard
                     .writeText(siteUrls)
