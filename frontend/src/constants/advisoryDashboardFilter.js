4a95b21f889e326c1fc447d62dd05a7698f17464
Ayumi Tanaka <ayumi@oxd.com>
2026-05-29 11:48:20 -0700

    CMS-1761: Update filter badge text

--- frontend/src/constants/advisoryDashboardFilter.js
+++ frontend/src/constants/advisoryDashboardFilter.js
@@ -3,11 +3,13 @@ export const PARK_FILTER_NAME = "park";
 
 export const TABLE_FILTER_LABELS = {
   "urgency.urgency": "Urgency",
-  "advisoryStatus.advisoryStatus": "Status",
+  "advisoryStatus.advisoryStatus": "Advisory status",
+  "accessStatus.accessStatus": "Resource status",
   advisoryDate: "Posting date",
   endDate: "End date",
   expiryDate: "Expiry date",
+  modifiedDate: "Last updated",
   title: "Headline",
   "eventType.eventType": "Event type",
-  associatedResources: "Associated Resource",
+  associatedResources: "Associated resource",
 };
