57a49b7b9b6b7bd9b1767a7096d5dd1993e7e5d3
Ayumi Tanaka <ayumi@oxd.com>
2026-06-26 11:58:48 -0700

    CMS-1861: Add rec id to fallback

--- frontend/src/components/advisories/composite/advisorySummaryView/AdvisorySummaryView.jsx
+++ frontend/src/components/advisories/composite/advisorySummaryView/AdvisorySummaryView.jsx
@@ -94,7 +94,7 @@ export default function AdvisorySummaryView({
             <div>
               {advisory.recreationResources.map((resource) => (
                 <div key={resource.id} className="mb-3">
-                  {resource.recResourceId && resource.isDisplayed ? (
+                  {resource.isDisplayed ? (
                     <a
                       href={`https://www.sitesandtrailsbc.ca/resource/${encodeURIComponent(resource.recResourceId)}`}
                       rel="noopener noreferrer"
@@ -108,7 +108,7 @@ export default function AdvisorySummaryView({
                       />
                     </a>
                   ) : (
-                    resource.resourceName
+                    `${resource.resourceName} (${resource.recResourceId})`
                   )}
                 </div>
               ))}
