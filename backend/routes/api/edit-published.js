1bf009c858cbe61ee6e167d1794ecc9f05732722
Ayumi Tanaka <ayumi@oxd.com>
2026-06-26 13:58:30 -0700

    CMS-1834: Update edit published endpoint

--- backend/routes/api/edit-published.js
+++ backend/routes/api/edit-published.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import * as SEASON_STATUS from "../../constants/seasonStatus.js";
 import parkRoutes from "./parks.js";
 
 const router = Router();
 
-// Reuse parks response for the Edit published page API.
-router.use("/", parkRoutes);
+// Reuse parks response for the Edit published page API, but only for published seasons.
+router.get("/", (req, res, next) => {
+  req.query = {
+    ...req.query,
+    seasonStatus: SEASON_STATUS.PUBLISHED,
+  };
+
+  parkRoutes.handle(req, res, next);
+});
 
 export default router;
