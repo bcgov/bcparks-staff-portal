285f34eef4e5a03cd6356421461acce22ca1c228
Ayumi Tanaka <ayumi@oxd.com>
2026-06-25 17:01:29 -0700

    CMS-1834: Change EditAndReview files name to Submit

--- frontend/src/router/pages/EditPublishedPage.jsx
+++ frontend/src/router/pages/EditPublishedPage.jsx
@@ -1,5 +1,16 @@
-import EditAndReview from "./EditAndReview";
 
 export default function EditPublishedPage() {
-  return <EditAndReview endpoint="/edit-published" />;
+  return (
+    <div className="container">
+      <div className="page edit-published">
+        <h3 className="fw-normal mb-4">Edit published dates</h3>
+
+        <div className="row">
+          <div className="col-md-6 col-lg-5">
+            {/* TODO: CMS-1836 - Add a park name search and a park table here */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
