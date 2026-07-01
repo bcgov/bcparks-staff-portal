b5ef36bb8e9e2d1e130eb86b2dae21bd3cef3e88
Ayumi Tanaka <90415568+ayumi-oxd@users.noreply.github.com>
2026-07-01 09:10:54 -0700

    CMS-1835: Add park name search (#649)
    * CMS-1835: Add park name search
    
    * CMS-1835: Change documentId to id

--- frontend/src/router/pages/EditPublishedPage.jsx
+++ frontend/src/router/pages/EditPublishedPage.jsx
@@ -1,5 +1,36 @@
+import { useMemo, useState } from "react";
+import { sortBy } from "lodash-es";
+
+import { useApiGet } from "@/hooks/useApi";
+import ParkSearch from "@/components/ParkSearch";
+import LoadingBar from "@/components/LoadingBar";
 
 export default function EditPublishedPage() {
+  const { data, loading, error } = useApiGet("/edit-published");
+  const parks = useMemo(() => data ?? [], [data]);
+
+  const [selectedParkOption, setSelectedParkOption] = useState(null);
+
+  const parkOptions = useMemo(
+    () =>
+      sortBy(
+        parks.map((park) => ({
+          value: park.id,
+          label: park.name,
+        })),
+        "label",
+      ),
+    [parks],
+  );
+
+  if (loading) {
+    return <LoadingBar />;
+  }
+
+  if (error) {
+    return <p>Error loading parks data: {error.message}</p>;
+  }
+
   return (
     <div className="container">
       <div className="page edit-published">
@@ -8,6 +39,11 @@ export default function EditPublishedPage() {
         <div className="row">
           <div className="col-md-6 col-lg-5">
             {/* TODO: CMS-1836 - Add a park name search and a park table here */}
+            <ParkSearch
+              options={parkOptions}
+              value={selectedParkOption}
+              onChange={setSelectedParkOption}
+            />
           </div>
         </div>
       </div>
