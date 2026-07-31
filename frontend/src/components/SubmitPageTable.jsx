b0efb034a648753dd4594b9b4a1bef6061dd4cb7
Mike Olund <mike@oxd.com>
2026-07-31 09:01:44 -0700

    CMS-1878: Hide winter fee dates when feature.hasWinterFeeDates is false

--- frontend/src/components/SubmitPageTable.jsx
+++ frontend/src/components/SubmitPageTable.jsx
@@ -118,6 +118,7 @@ function DateTableRow({
   groupedDateRanges,
   currentYear,
   showCalculatedDateTypeLabels = false,
+  hasWinterFeeDates = false,
 }) {
   if (!currentYear || !groupedDateRanges) return null;
 
@@ -130,6 +131,12 @@ function DateTableRow({
 
   return sortedDateTypes.map(([dateTypeName, yearsObj]) => {
     const dateTypeNumber = getDateTypeNumber(yearsObj);
+
+    // hasWinterFeeDates flag overrides db dateRange existence
+    if (!hasWinterFeeDates && dateTypeNumber === DATE_TYPE.WINTER_FEE) {
+      return null;
+    }
+
     const showCalculatedDateTypeLabel =
       showCalculatedDateTypeLabels && dateTypeNumber === DATE_TYPE.WINTER_FEE;
 
@@ -157,6 +164,7 @@ DateTableRow.propTypes = {
   groupedDateRanges: PropTypes.object,
   currentYear: PropTypes.number,
   showCalculatedDateTypeLabels: PropTypes.bool,
+  hasWinterFeeDates: PropTypes.bool,
 };
 
 function getDisplayGroupedDateRanges(groupedDateRanges, showWinterFeeDates) {
@@ -371,6 +379,7 @@ function FeaturesByFeatureTypeWithAreas({
                       groupedDateRanges={displayGroupedDateRanges}
                       currentYear={regularSeason.operatingYear}
                       showCalculatedDateTypeLabels={true}
+                      hasWinterFeeDates={parkFeature.hasWinterFeeDates}
                     />
                     {isApprover && regularSeason.hasNotes && (
                       <InternalNotesRow seasonId={regularSeason.id} />
