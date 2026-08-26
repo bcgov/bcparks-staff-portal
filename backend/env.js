d61bb021398183d15e4fffcdab2b34c93f470872
Duncan MacKenzie <duncan@oxd.com>
2026-08-25 19:05:13 -0700

    CMS-000: Hardcode absolute backend env file path

--- backend/env.js
+++ backend/env.js
@@ -2,7 +2,7 @@ import dotenvx from "@dotenvx/dotenvx";
 
 // Load local .env files, if they exist.
 dotenvx.config({
-  path: [".env", ".env.local"],
+  path: ["/app/backend/.env", "/app/backend/.env.local"],
   overload: true,
   // Ignore missing file warnings: files are just for dev
   // and any files loaded will print a success message
